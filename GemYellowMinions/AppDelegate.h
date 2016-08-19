01091af3afc9a105f3373822b6438d8b6a36bc09
GemShi <GemShi@anxiangkejidemacbook-pro.local>
2016-08-19 11:39:56 +0800

    Initial Commit

---
+++ GemYellowMinions/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GemYellowMinions
+//
+//  Created by GemShi on 16/8/19.
+//  Copyright © 2016年 GemShi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
